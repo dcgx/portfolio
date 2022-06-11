488f7b0dccb4c8cc66347a8eed8f11a070409f2f
Diego Castillo <diego.castilloguajardo@gmail.com>
2022-06-11 01:56:47 -0400

    add aside nav links

--- src/layouts/components/aside.js
+++ src/layouts/components/aside.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 //w-48 fixed bottom-0 left-0
 
-import { BsGithub, BsTwitter, BsLinkedin } from 'react-icons/bs'
+import { BsGithub, BsTwitter, BsLinkedin, BsBriefcase } from 'react-icons/bs'
 
 const StyledAside = styled.footer`
   width: var(--aside-width);
@@ -16,8 +16,36 @@ const StyledAside = styled.footer`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: flex-end;
     padding: 20px 0;
+
+    .nav-links {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      a {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-weight: 600;
+        cursor: pointer;
+        &:hover {
+          border-radius: 20px;
+          border: none;
+          background: #f3f4f6;
+        }
+        span {
+          font-size: 15px;
+        }
+      }
+    }
+    .social-links {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+    }
     svg {
       margin-bottom: 12px;
     }
@@ -32,9 +60,29 @@ const Aside = () => {
   return (
     <StyledAside>
       <div className="inner ">
-        <BsGithub size={30} />
-        <BsTwitter size={30} />
-        <BsLinkedin size={30} />
+        <div className="nav-links">
+          <a>
+            <BsBriefcase size={25} />
+            <span>Sobre mí</span>
+          </a>
+          <a>
+            <BsBriefcase size={25} />
+            <span>Experiencia</span>
+          </a>
+          <a>
+            <BsBriefcase size={25} />
+            <span>Portafolio</span>
+          </a>
+          <a>
+            <BsBriefcase size={25} />
+            <span>Educación</span>
+          </a>
+        </div>
+        <div className="social-links">
+          <BsGithub size={30} />
+          <BsTwitter size={30} />
+          <BsLinkedin size={30} />
+        </div>
       </div>
     </StyledAside>
   )
